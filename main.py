192110f241544dc38f5923df757c4998ded72520
SHANIB-C-K <ckshanib8@gmail.com>
2025-02-19 12:28:41 +0530

    `Added camera flipping, RGB conversion, and exit text to main.py`

--- main.py
+++ main.py
@@ -15,6 +15,11 @@ while True:
         break
 
     # Camera showing section
+    frame = cv.flip(frame, 1)
+    # Convert the BGR image to RGB image
+    rgb_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    # exit text on window
+    cv.putText(frame, "Press 'q' to exit", (10, 20), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
     cv.imshow("HandlandMark detection", frame)
 
     # set if i clicked q button then close the video window
